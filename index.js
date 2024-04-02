88c1e00bea8910272399b9f909b43ba8f6c8785b
ABDELAZIZ AITMOUSSA <57140192+azizou98@users.noreply.github.com>
2024-04-02 17:50:40 +0000

    commit 02-04 changes

--- index.js
+++ index.js
@@ -27,13 +27,26 @@ app.get("/api/hello", function (req, res) {
 app.get('/api/:date?',function(req,res){
    const datestring = req.params.date;
    try{
-     const parsedate = new Date(datestring);     
-       console.log('date parsed',parsedate)
-      // console.log('date parsed',parsedate.getTime())
-
-     if(!(parsedate === 'Invalid Date')){
+    function sendDate(parsedate){
+      const options = {
+        weekday: 'short', // Fri
+        year: 'numeric',  // 2015
+        month: 'long',   // December
+        day: 'numeric',  // 25
+        timeZone: 'GMT'  // GMT timezone
+      };
+      const houroptions= {
+        hour: '2-digit', // 00 (padded)
+        minute: '2-digit', // 00 (padded)
+        second : '2-digit',
+        hour12: false  ,  // 25
+        timeZone: 'UTC'
+      }
+    
       unixtimestamp =parsedate.getTime() ;
-      utcdate = parsedate.getUTCDate() ;
+      utcdate = parsedate.toLocaleDateString('en-US',options) +
+       ' '+ 
+       parsedate.toLocaleTimeString('en-US',houroptions)+ ' GMT';
     
       res.json(
         {
@@ -41,10 +54,29 @@ app.get('/api/:date?',function(req,res){
           utc : utcdate
         }
       )
-     }else{
-      console.log('date not valid');
-      return res.status(400).send('Invalid date format');
      }
+    
+     let parsedate = new Date(datestring);     
+       console.log('date parsed',parsedate);
+      // console.log('date parsed',parsedate.getTime())
+
+     if(isNaN(parsedate.getTime())){
+      if((datestring)=>{
+        const parsedValue = parseInt(datestring, 10);
+        return !isNaN(parsedValue) && parsedValue >= 0; 
+      }){
+
+          parsedate=new Date(parseInt(datestring, 10)*1);
+          console.log('sema unix timestamp',parsedate);
+          sendDate(parsedate);
+        }else{
+          console.log('date not valid');
+       return res.status(400).json({error:'Invalid Date'});
+        }
+     }
+     else{
+      sendDate(parsedate);
+      }
    }
    catch(error){
 
